292077d2db73faa1ab3ddd31286c24ca13138bcb
Snow <termwan.c@gmail.com>
2025-04-24 22:37:02 -0700

    create simple math operation

---
+++ lesson03/calculator.js
@@ -0,0 +1,16 @@
+let num1 = 1;
+let num2 = 5;
+let num3 = 10;
+let num4 = 20;
+
+let add = num1 + num2;
+let subtract = num4 - num3;
+let multiply = num2 * num3;
+let divide = num4 / num2;
+let complexOperation = (num1 + num2) * (num3 - num4);
+
+console.log("Result is" + " " + add);
+console.log("Result is" + " " + subtract);
+console.log("Result is" + " " + multiply);
+console.log("Result is" + " " + divide);
+console.log("Result is" + " " + complexOperation);
