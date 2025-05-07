76305563547fcdf333bcbca01d4ffc740600f89e
Snow <termwan.c@gmail.com>
2025-05-06 20:17:25 -0700

    assignment 4.2 added

---
+++ lesson4/exercise-two.js
@@ -0,0 +1,7 @@
+// Take a sentence of at least five words and a period.
+let sentence = "I like my matcha with oat milk.";
+console.log(sentence);
+
+// Make an array from that sentence and console.log the third word.
+let words = sentence.split(" ");
+console.log(words[2]);
