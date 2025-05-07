6ef7fe65a4241fe0401fbb3f1f958eb67f9754a7
Snow <termwan.c@gmail.com>
2025-05-06 20:43:14 -0700

    assignment 4.3 added

---
+++ lesson4/exercise-three.js
@@ -0,0 +1,6 @@
+// Use a for loop to console log all of the elements plus the index and a comma
+let alternativeMilk = ["almond", "oat", "coconut", "pistachio", "soy"];
+
+for (let i = 0; i < alternativeMilk.length; i++) {
+  console.log(i + 1 + "," + alternativeMilk[i]);
+}
